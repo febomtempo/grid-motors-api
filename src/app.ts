47fc43a43b530f35f13d1bcdcce4da7be5580619
Felipe <febomtempo@hotmail.com>
2023-04-05 08:42:03 -0300

    feat: add mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import cors from 'cors';
+import connectMongo from './database/connect';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+connectMongo();
 
 app.get('/api/v1/', (req: Request, res: Response) => {
   res.status(200).json({

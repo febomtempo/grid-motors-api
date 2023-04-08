84c2b1af284fc4e64493a09eeaf3dbbc98723622
Felipe <febomtempo@hotmail.com>
2023-04-07 23:31:10 -0300

    feat: add protect middleware to user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,6 +5,7 @@ import { UserService } from '../user/UserService';
 import { UserRepository } from '../user/UserRepository';
 import { validate } from '../validation/Validator';
 import { updateUserSchema, userSchema } from '../user/UserValidation';
+import { protect } from '../auth/AuthController';
 
 const userRepository = new UserRepository(User);
 const userService = new UserService(userRepository);
@@ -13,11 +14,12 @@ const userController = new UserController(userService);
 const userRoutes: Router = express.Router();
 
 userRoutes.post('/', validate(userSchema), userController.createUser);
-userRoutes.get('/', userController.findAllUsers);
-userRoutes.get('/:id', userController.findUserById);
-userRoutes.delete('/:id', userController.deleteUserById);
+userRoutes.get('/', protect, userController.findAllUsers);
+userRoutes.get('/:id', protect, userController.findUserById);
+userRoutes.delete('/:id', protect, userController.deleteUserById);
 userRoutes.put(
   '/:id',
+  protect,
   validate(updateUserSchema),
   userController.updateUserById
 );

4b140b54c0f888879d3206213b0f7fa1c2e6d1fe
Felipe <febomtempo@hotmail.com>
2023-04-08 15:55:04 -0300

    car endpoints

---
+++ src/routes/carRoutes.ts
@@ -0,0 +1,20 @@
+import express, { Router } from 'express';
+import { CarController } from '../car/CarController';
+import Car from '../car/CarModel';
+import { CarService } from '../car/CarService';
+import { CarRepository } from '../car/CarRepository';
+import { protect } from '../auth/AuthController';
+
+const carRepository = new CarRepository(Car);
+const carService = new CarService(carRepository);
+const carController = new CarController(carService);
+
+const carRoutes: Router = express.Router();
+
+carRoutes.post('/', protect, carController.createCar);
+carRoutes.get('/', protect, carController.findAllCars);
+carRoutes.get('/:id', protect, carController.findCarById);
+carRoutes.delete('/:id', protect, carController.deleteCarById);
+carRoutes.put('/:id', protect, carController.updateCarById);
+
+export default carRoutes;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import userRoutes from './userRoutes';
 import authRoutes from './authRoutes';
+import carRoutes from './carRoutes';
 
 const routes = Router();
 const prefixRoutes = '/api/v1';
 
 routes.use(`${prefixRoutes}/user`, userRoutes);
+routes.use(`${prefixRoutes}/car`, carRoutes);
 routes.use(`${prefixRoutes}/authenticate`, authRoutes);
 
 export default routes;

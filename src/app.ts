03e96855ae165ae1940d11c4b17398a51483d2b8
Felipe <febomtempo@hotmail.com>
2023-04-10 11:23:51 -0300

    feat: added swagger

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import { Request, Response } from 'express';
 import cors from 'cors';
 import connectMongo from './database/connect';
 import routes from './routes';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDoc from '../src/swagger/swagger.json';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/v1/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 connectMongo();
 
